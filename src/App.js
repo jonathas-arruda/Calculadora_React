9bfea6a990a2b00b8f60938975ea5f3a8490b9c6
Jonathas Arruda <jonathasarruda@outlook.com>
2022-01-21 10:00:19 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './components/UI/Card'
+
+const App = () => {
+  return (
+    <Card Classname=''>
+      <div className="App">
+        <h2>Start</h2>
+      </div>
+    </Card>
+  );
+}
+
+export default App;

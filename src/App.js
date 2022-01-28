d347808a92350d17b1ecc9c8572cca9d552402ac
Jonathas Arruda <jonathasarruda@outlook.com>
2022-01-28 18:31:55 -0300

    Changes in the first commit, and adding new buttons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
+import { useState } from "react";
 import "./App.css";
-import Board from "./components/Board";
 
 const App = () => {
+  const [result, setResult] = useState("");
   return (
-    <Board className="board">
-      <div className="App">
-        <h2>Start</h2>
-        <h2>Start</h2>
-        <h2>Start</h2>
-        <h2>Start</h2>
-        <h2>Start</h2>
-        <h2>Start</h2>
-        <h2>Start</h2>
-        <h2>Start</h2>
-        <h2>Start</h2>
+    <div>
+      <div className="container">
+        <form>
+          <input type="text" value={result} />
+        </form>
+        <div className="keypad">
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
       </div>
-    </Board>
+    </div>
   );
 };
 
